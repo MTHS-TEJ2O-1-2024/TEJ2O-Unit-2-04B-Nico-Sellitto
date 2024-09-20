5fe5e58fd695d31499fc2b9bbdbe8e69a35623ab
Nico-Sellitto <nico.sellitto@mths.ca>
2024-09-20 14:53:39 +0000

    made temperature display

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Nico Sellitto
+Created on: Sep 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+temp = temperature
 
-display.scroll("Hello, World!")
+with True:
+if button_a.is_pressed:{
+
+    display.scroll('the temperature is:')
+    display.scroll(temp)
+
+}
